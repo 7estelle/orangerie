fb8595dd9eb40f0f77a7ced57551139f5ea87cd0
Néo <neo@nefture.com>
2023-01-06 15:07:11 +0100

    feat: routing & featching data with json-server

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello les Oranges</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident, totam!</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
